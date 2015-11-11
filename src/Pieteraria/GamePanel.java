bd4ed53459c8c3ced906f109088fc18540375169
PieterEbbers <4307@pierson.nl>
2015-11-11 12:08:40 +0100

    adf
    afd

--- src/Pieteraria/GamePanel.java
+++ src/Pieteraria/GamePanel.java
@@ -20,15 +20,22 @@ import javax.swing.JPanel;
 
 public class GamePanel extends JPanel implements Runnable, KeyListener
 {
-	private int worldWidth = 20, worldHeight = 30;
 	private Thread thread;
+	
 	private boolean running;
+	private boolean left, right, up, down;
+	private boolean canFire = true, isFiring = false;
+	
+	private long timer, fireTimer = 0;
+	
 	private Graphics2D g;
 
 	private BufferedImage image;
 	private int screenWidth = 680, screenHeight = 640;
 	
-	private  Block[][] block = new Block[worldWidth][worldHeight];
+	private float playerSpeed = 1.3f;
+	
+	private  Player p;
 			
 	public GamePanel()
 	{
@@ -48,22 +55,6 @@ public class GamePanel extends JPanel implements Runnable, KeyListener
 		}
 	}
 	
-	@Override public void keyTyped(KeyEvent e) {}
-
-	@Override
-	public void keyPressed(KeyEvent e) 
-	{
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void keyReleased(KeyEvent e)
-	{
-		// TODO Auto-generated method stub
-		
-	}
-
 	@Override
 	public void run() 
 	{
@@ -72,30 +63,23 @@ public class GamePanel extends JPanel implements Runnable, KeyListener
 		running = true;
 		image = new BufferedImage(screenWidth, screenHeight, BufferedImage.TYPE_INT_RGB);
 		g = (Graphics2D) image.getGraphics();
-		
-		for(int col = 0; col < block.length; col++)
-		{
-			for(int row = 0; row < block[0].length; row++)
-			{
-				block[col][row] = new Block(col * 10,row * 10, 10, 10);
-			}
-		}
-		
+	
+		p = new Player(screenWidth / 2, screenHeight / 6 * 5, 7);
+	
 		while(running)
 		{
-		//GAMELOOP
-			//render
 			drawScreen();
 			render();
 			update();
-			//update
 		}
 	}
 
 	private void update() 
 	{
 		// TODO Auto-generated method stub
-		
+		timer = System.currentTimeMillis();
+		movement();
+		p.update();
 	}
 
 	private void render() 
@@ -104,17 +88,7 @@ public class GamePanel extends JPanel implements Runnable, KeyListener
 		g.setColor(new Color(50,60,200));
 		g.fillRect(0, 0, screenWidth, screenHeight);
 		
-		//Grid jwz
-		for(int col = 0; col < block.length; col++)
-		{
-			for(int row = 0; row < block[0].length; row++)
-			{
-				block[col][row].render(g);
-			}
-		}
-		
-		
-		//Objects
+		p.render(g);
 	}
 
 	private void drawScreen() 
@@ -123,5 +97,92 @@ public class GamePanel extends JPanel implements Runnable, KeyListener
 		g2.drawImage(image, 0, 0, null);
 		g2.dispose();
 	}
+	
+	private void movement()
+	{
+		if(left)
+		{
+			moveX(-playerSpeed);
+		}
+		if(right)
+		{
+			moveX(playerSpeed);
+		}
+		if(up)
+		{
+			moveY(playerSpeed)
+		}
+		if(down)
+		{
+			moveY(-playerSpeed)
+		}
+		if(isFiring && !canFire && fireTimer == 0)
+		{
+			fireTimer = System.currentTimeMillis();
+		}
+		int reloadTime = timer - fireTimer;
+		if(reloadTime >= 400 && isFiring && !canFire && fireTimer != 0)
+		{
+			canFire = true;
+		}
+		if(isFiring && canFire)
+		{
+			//add bullet
+			
+			fireTimer = 0;
+		}
+	}
 
+	
+	@Override public void keyTyped(KeyEvent e) {}
+
+	@Override
+	public void keyPressed(KeyEvent e) 
+	{
+		if(e.getKeyCode() == KeyEvent.VK_A || e.getKeyCode() == KeyEvent.VK_LEFT)
+		{
+			left = true;
+		}
+		if(e.getKeyCode() == KeyEvent.VK_D || e.getKeyCode() == KeyEvent.VK_RIGHT)
+		{
+			right = true;
+		}
+		if(e.getKeyCode() == KeyEvent.VK_W || e.getKeyCode() == KeyEvent.VK_UP)
+		{
+			up = true;
+		}
+		if(e.getKeyCode() == KeyEvent.VK_S || e.getKeyCode() == KeyEvent.VK_DOWN)
+		{
+			down = true;	
+		}
+		if(e.getKeyCode() == KeyEvent.VK_SPACE)
+		{
+			isFiring = true;
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e)
+	{
+		if(e.getKeyCode() == KeyEvent.VK_A || e.getKeyCode() == KeyEvent.VK_LEFT)
+		{
+			left = false;
+		}
+		if(e.getKeyCode() == KeyEvent.VK_D || e.getKeyCode() == KeyEvent.VK_RIGHT)
+		{
+			right = false;
+		}
+		if(e.getKeyCode() == KeyEvent.VK_W || e.getKeyCode() == KeyEvent.VK_UP)
+		{
+			up = false;
+		}
+		if(e.getKeyCode() == KeyEvent.VK_S || e.getKeyCode() == KeyEvent.VK_DOWN)
+		{
+			down = false;	
+		}
+		if(e.getKeyCode() == KeyEvent.VK_SPACE)
+		{
+			isFiring = false;
+		}
+	}
 }
