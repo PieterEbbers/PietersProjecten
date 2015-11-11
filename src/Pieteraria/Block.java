f72d60ac5364bba7c10642b22e6fc786569eff7d
PieterEbbers <4307@pierson.nl>
2015-11-11 12:08:36 +0100

    Player.java
    e

--- src/Pieteraria/Block.java
+++ src/Pieteraria/Block.java
@@ -4,24 +4,46 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
 
-public class Block
+public class Player
 {
 
-	private int x,y,width,height;
+	private double x,y,width,height;
 	
-	public Block(int x, int y, int width, int height)
+	public Player(double x, double y, int r)
 	{
 		this.x = x;
 		this.y = y;
-		this.width = width;
-		this.height = height;
+		this.r = r;
 	}
 	
 	public void render(Graphics2D g)
 	{
 		g.setColor(Color.MAGENTA);
-		g.drawRect(x,y,width,height);
+		g.drawRect(x - r,y - r,width,height);
 		g.setColor(Color.GREEN);
-		g.fillRect(x, y, width, height);
+		g.fillRect(x - r, y -r, width, height);
+	}
+	
+	private void moveX(float speed)
+	{
+		x += speed;	
+	}
+	
+	private void moveY(float speed)
+	{
+		y -= speed;
+	}
+	
+	public double getX()
+	{
+		return x;
+	}
+	public double getY()
+	{
+		return y;
+	}
+	public double getR()
+	{
+		return r;
 	}
 }
