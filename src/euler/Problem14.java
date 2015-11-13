21578106834389914a7a517e06f87054a45302a8
PieterEbbers <4307@pierson.nl>
2015-11-13 13:04:54 +0100

    m
    vb

--- src/euler/Problem14.java
+++ src/euler/Problem14.java
@@ -4,8 +4,25 @@ public class Problem14 {
 	public static void main(String[] args)
 	{
 		long startTime = System.currentTimeMillis();
-
+		long number;
+		long terms = 0;
 		
+		for(long startNum = 2; startNum < 1000000; startNum++)
+		{
+			number = startNum;
+			while(number != 1)
+			{
+				terms++;
+				if(number % 2 == 0)
+				{
+					number = number / 2;	
+				} 
+				else
+				{
+					number = (number * 3) + 1;
+				}
+			}
+		}
 
 		double programRunTime = System.currentTimeMillis() - startTime;
 		System.out.println("It took: " + programRunTime / 1000 + " seconds to find the answer.");
